d5fa6add897ca39456359dc42808cf05d98fcd16
dibyendud@tranistics.com <dibyendud@tranistics.com>
2020-09-14 19:35:28 +0530

    Added new feature

--- HelloGit/HelloGit/HelloGit/Program.cs
+++ HelloGit/HelloGit/HelloGit/Program.cs
@@ -11,6 +11,7 @@ namespace HelloGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git");
+            Console.WriteLine("Added new feature");
             Console.ReadLine();
         }
     }
